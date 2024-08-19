7522b4ba8dc697fab9cd218ebe97e6c5eb23742b
Maleek6526 <adewalemaleekayobami2528@gmail.com>
2024-08-19 17:40:58 +0100

    Add Arithmetic2.java file

---
+++ Arithmetic2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class VolumeOfATriangle{
+		public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter the first integer: ");
+		int first = input.nextInt();
+
+		System.out.print("Enter the second integer: ");
+		int second = input.nextInt();
+
+	
+	}
+	}

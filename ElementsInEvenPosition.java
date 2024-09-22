b78bdd433c7f946c7f04e1c9611bdc01dd884502
Adewale Maleek Ayobami <adewalemaleekayobami2528@gmail.com>
2024-09-22 10:15:06 +0100

    Elements In The Even Position

---
+++ ElementsInEvenPosition.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class ElementsInEvenPosition{
+
+	public static void main(String[] args){
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Enter the number of list you want to create: ");
+		int listCreated = scanner.nextInt();
+
+		int [] myArrayLists = new int [listCreated];
+
+		for(int count = 0; count < myArrayLists.length; count++){
+
+			System.out.print("Enter the elements: ");
+
+			myArrayLists[count] = scanner.nextInt();
+
+		}
+
+		getElementsInAList(myArrayLists);
+
+		
+		
+
+	}
+
+public static void getElementsInAList(int[] arrayList){
+
+	for(int count = 0; count < arrayList.length; count++){	
+
+		if(count % 2 != 0){								System.out.print(arrayList[count]);
+		}
+	}	
+}
+
+}

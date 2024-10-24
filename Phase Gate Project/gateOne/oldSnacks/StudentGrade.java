30c97681d28e82d45a5883d0a3f99cef8bc42b73
Adewale Maleek Ayobami <adewalemaleekayobami2528@gmail.com>
2024-10-24 19:12:37 +0100

    StudentGrade.java

---
+++ Phase Gate Project/gateOne/oldSnacks/StudentGrade.java
@@ -0,0 +1,173 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class StudentGrade{
+
+	public static void main(String[] args){
+
+		Scanner scanner = new Scanner(System.in);
+
+	try{
+
+		System.out.print("How many number of student do you have? ");
+		int numberOfStudents = scanner.nextInt();
+
+		System.out.print("How many subjects do they offer? ");
+		int numberOfSubjects = scanner.nextInt();
+
+		if(numberOfStudents > 0){
+	
+			if(numberOfSubjects > 0){
+
+				getStudentChart(numberOfStudents, numberOfSubjects);
+
+			}
+
+			else{
+
+				System.out.println("Invalid Input");
+
+			}
+
+		}
+
+		else{
+
+			System.out.println("Oga Haffa you now!!");
+
+		}
+
+		
+
+	}
+
+	catch(Exception e){
+
+		System.out.println("Invalid input");
+
+	}
+
+	}
+
+	public static int[][] getInputCollected(int num, int sub){
+
+		Scanner scanner = new Scanner(System.in);
+
+		String savingInputDisplay = """ 
+
+				Saving >>>>>>>>>>>>>>>>>>>>>
+				Saved successfully
+
+				""";
+
+		System.out.println(savingInputDisplay);
+
+
+		int[][] studentGrade = new int [num][sub];
+	
+		for(int index = 0; index < studentGrade.length; index++){
+			for(int count = 0; count < studentGrade[index].length; count++){
+
+				System.out.println("Enter score for student " + (index+1) + ": ");
+				System.out.println("Enter student score in Subject" + (count+1) + ": ");
+
+				studentGrade[index][count] = scanner.nextInt();
+
+	
+			String storingInputDisplay = """ 
+
+				Saving >>>>>>>>>>>>>>>>>>>>>
+				Saved successfully
+
+			""";
+
+		System.out.println(storingInputDisplay);
+
+			}
+			System.out.println();
+
+		}
+			return studentGrade;
+	}
+
+
+	public static void getStudentChart(int num, int sub){
+
+		int[] positioning = new int[num];
+
+		int[][]scoreInputs = getInputCollected(num, sub);
+
+		System.out.println("==============================================================================================================================================");
+
+			System.out.print("SUBJECT \t");
+
+		for(int index = 0; index < sub; index++){
+
+
+			System.out.print("SUB"+ (index+1) + " \t ");
+
+		}
+
+			System.out.print("TOT \t AVE \t POS");
+
+			System.out.println();
+
+		System.out.println("===============================================================================================================================================");
+		
+		for(int index = 0; index < scoreInputs.length; index++){
+
+			int sum = 0;
+			float average = 0;
+
+			System.out.print("STUDENT "+ (index+1) );	
+
+			for(int count = 0; count < scoreInputs[index].length; count++){
+
+				sum += scoreInputs[index][count];
+				System.out.print(" \t " + scoreInputs[index][count]);
+
+			}
+				positioning[index] = sum;
+
+				average = (float) sum / sub;
+			System.out.print(" \t"+sum);
+			System.out.printf(" \t%.2f", average);
+			System.out.print(" \t  "+ positioning[index]);
+			System.out.println();
+
+		}
+
+		System.out.println("==============================================================================================================================================");
+
+
+
+		for(int index = 1; index < positioning.length; index++){
+
+			int temp = 0;
+	
+			for(int count = 1; count < positioning.length; count++){
+
+				if(positioning[count] < positioning[count-1]){
+	
+					temp = positioning[count];
+
+					positioning[count] = positioning[count-1];
+				
+					positioning[count-1] = temp;
+
+				}
+
+				
+
+			}
+		}
+
+
+		System.out.println(Arrays.toString(positioning));
+
+
+		
+
+	}
+
+
+}

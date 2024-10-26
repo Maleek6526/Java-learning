7f09ba2e2a8138b357c8627a00272aa29b387aee
Adewale Maleek Ayobami <adewalemaleekayobami2528@gmail.com>
2024-10-26 11:11:46 +0100

    phoneBook

---
+++ Phase Gate Project/gateOne/phoneBook/PhoneBookApp.java
@@ -0,0 +1,74 @@
+public class PhoneBookApp{
+
+	private String name;
+
+	private String address;
+
+	private String telephone;
+
+	private String email;
+
+
+
+public void setName(String name){
+
+	this. name = name;
+
+}
+
+
+public String getName(){
+
+	return name;
+
+}
+
+
+public void setAddress(String address){
+
+	this. address = address;
+
+}
+
+
+public String getAddress(){
+
+	return address;
+
+}
+
+
+public void setTelephone(String telephone){
+
+	this. telephone = telephone;
+
+}
+
+
+public String getTelephone(){
+
+	return telephone;
+
+}
+
+
+public void setEmail(String email){
+
+	this. email = email;
+
+}
+
+
+public String getEmail(){
+
+	return email;
+
+}
+
+
+
+
+
+
+
+}

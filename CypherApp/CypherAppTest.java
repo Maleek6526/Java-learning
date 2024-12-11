5118655293daecb19a9dba39928a4707ac52087a
Adewale Maleek Ayobami <adewalemaleekayobami2528@gmail.com>
2024-12-11 13:43:16 +0100

    CypherApp

---
+++ CypherApp/CypherAppTest.java
@@ -0,0 +1,20 @@
+package CypherApp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CypherAppTest {
+    @Test
+    public void checkIfTheCyptherAppWorks(){
+        Encryption encryption = new Encryption();
+        assertEquals(encryption.getEncryption("CODEDAMN", 3), "FRGHGDPQ");
+    }
+    @Test
+    public void checkIfTheCyptherDecryptionAppWorks(){
+        Encryption encryption = new Encryption();
+        assertEquals(encryption.getDecryption("FRGHGDPQ", 3), "CODEDAMN");
+    }
+
+}
